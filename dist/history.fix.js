22ab850d7275e1a67796845b0c9297c510f7e6e3
iamso <so@dev.so>
2025-02-18 09:51:00 +0100

    0.7.0

--- dist/history.fix.js
+++ dist/history.fix.js
@@ -3,6 +3,6 @@
  *
  * Made with ❤ by Steve Ottoz so@dev.so
  *
- * Copyright (c) 2023 Steve Ottoz
+ * Copyright (c) 2025 Steve Ottoz
  */
-!function(t){if(!t&&!t.pushState)return!1;var e=t.pushState,n=t.replaceState;function a(t,e){var n=document.createEvent("CustomEvent");n.initCustomEvent(t+"state",!0,!0,e),window.dispatchEvent(n)}t.pushState=function(n,u){var r=e.apply(t,arguments);return u&&(document.title=u),a("push",n),r},t.replaceState=function(e,u){var r=n.apply(t,arguments);return u&&(document.title=u),a("replace",e),r}}(window.history);
+!function(t){if(!t&&!t.pushState)return!1;var e=t.pushState,n=t.replaceState;function a(t,e){var n=new CustomEvent(t+"state",{detail:e});window.dispatchEvent(n)}t.pushState=function(n,r){var u=e.apply(t,arguments);return r&&(document.title=r),a("push",n),u},t.replaceState=function(e,r){var u=n.apply(t,arguments);return r&&(document.title=r),a("replace",e),u}}(window.history);
